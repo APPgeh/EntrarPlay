8a7571aaf55d5d6fb9c0c55a4052466a83f7963e
APPgeh <playgeh5496@gmail.com>
2025-06-29 07:53:35 -0300

    Atualizar o script.js

--- EntrarPlayprincipal/js/script.js
+++ EntrarPlayprincipal/js/script.js
@@ -1,14 +1,17 @@
-<script>
-  (function () {
-    var script = document.createElement('script');
-    script.src = 'https://groleegni.net/401/9474483';
-    script.defer = true;
-    script.onload = function () {
-      console.log('Script carregado com sucesso');
-    };
-    script.onerror = function () {
-      console.error('Erro ao carregar o script');
-    };
-    document.head.appendChild(script);
-  })();
-</script>
+window.onload = function(){    
+    load();
+}
+
+
+function load(){
+    var menuBtn = document.getElementById("menu-btn");
+    var navLinks = document.getElementById("nav-links");
+
+
+
+    menuBtn.addEventListener('click',()=>{
+        navLinks.classList.toggle('mobile-menu')
+    })
+
+
+}
